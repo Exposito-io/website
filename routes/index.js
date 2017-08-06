c01657cbb7a7363a5ebe260e62fa7911039d8b07
Mathew Cormier <mathew.corm@gmail.com>
2017-08-06 12:34:28 -0400

    Random homepages

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,36 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
-/* GET home page. */
+const PAGES = [
+  '/homea',
+  '/homeb',
+  //'/homec'
+]
+
 
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
-});
+  //res.render('index', { title: 'Express' })
+  res.redirect(getHomepage())
+})
+
+router.get('/homea', function(req, res) {
+  res.render('home-a', { title: 'A' })
+})
+
+router.get('/homeb', function(req, res) {
+  res.render('home-b', { title: 'B' })
+})
+
+
+
+function getHomepage() {
+  return PAGES[getRandomInt(0, PAGES.length)]
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor((Math.random() * (max - min)) + min)
+}
 
 module.exports = router;
