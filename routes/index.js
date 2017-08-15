0ee02cd09a200162dda6770070bf80831f38d50b
Mathew <mathew.corm@gmail.com>
2017-08-15 11:30:59 -0400

    Add config + recaptcha in newsletter

--- routes/index.js
+++ routes/index.js
@@ -1,42 +1,54 @@
 var express = require('express')
 var router = express.Router()
+var recaptcha = require('express-recaptcha')
+var config = require('config')
+
+recaptcha.init(config.get('recaptchaSiteKey'), config.get('recaptchaSecret'))
+
 
 const PAGES = [
-  '/homea',
-  //'/homeb',
-  //'/homec'
+    '/homea',
+    //'/homeb',
+    //'/homec'
 ]
 
 
-router.get('/', function(req, res) {
-  //res.render('index', { title: 'Express' })
-  res.redirect(getHomepage())
+router.get('/', function (req, res) {
+    //res.render('index', { title: 'Express' })
+    res.redirect(getHomepage())
 })
 
 
-router.get('/homea', function(req, res) {
-  res.render('home-a', { title: 'A' })
+router.get('/homea', function (req, res) {
+    res.render('home-a', { title: 'A' })
 })
 
-router.get('/homeb', function(req, res) {
-  res.render('home-b', { title: 'B' })
+router.get('/homeb', function (req, res) {
+    res.render('home-b', { title: 'B' })
 })
 
 
-router.post('/newsletter', function(req, res) {
-  // TODO
-  res.render('home-a', { title: 'A' })
+router.post('/newsletter', function (req, res) {
+    // TODO
+    recaptcha.verify(req, function (error) {
+        if (error)
+            return res.send('0')
+
+
+
+    })
+
 })
 
 
 function getHomepage() {
-  return PAGES[getRandomInt(0, PAGES.length)]
+    return PAGES[getRandomInt(0, PAGES.length)]
 }
 
 function getRandomInt(min, max) {
-  min = Math.ceil(min)
-  max = Math.floor(max)
-  return Math.floor((Math.random() * (max - min)) + min)
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor((Math.random() * (max - min)) + min)
 }
 
 module.exports = router
