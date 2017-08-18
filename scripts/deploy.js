e9ef546e0a962471f87c3e725543209c4e1e756c
Mathew <mathew.corm@gmail.com>
2017-08-18 16:42:34 -0400

    get version from package.json

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,6 +1,7 @@
 const { execFile } = require('child-process-promise')
 
 
+
 // npm version patch
 // docker build -t gcr.io/quantal-152414/exposito-website:v0.0.3 -f deploy/Dockerfile .
 // gcloud docker -- push gcr.io/quantal-152414/exposito-website:v0.0.3
@@ -8,8 +9,11 @@ const { execFile } = require('child-process-promise')
 
 
 async function main() {
-    let versionTag = 'gcr.io/quantal-152414/exposito-website:v0.0.3'
     let versionResults = await execFile('npm', ['version', 'patch'])
+
+    let config = require('../package.json')
+    let versionTag = `gcr.io/quantal-152414/exposito-website:v${config.version}`
+
     await execFile('docker', ['build', '-t', versionTag, '-f', 'deploy/Dockerfile', '.'])
     await execFile('gcloud', ['docker', '--', 'push', versionTag])
     await execFile('kubectl', ['set', 'image', 'deployment', 'exposito-website', 'exposito-website=' + versionTag])
