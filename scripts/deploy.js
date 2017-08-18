dde22d62dd0cd48452c822fdf601eb45d959c28d
Mathew <mathew.corm@gmail.com>
2017-08-18 16:40:19 -0400

    add kubectl commande to deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,6 +4,7 @@ const { execFile } = require('child-process-promise')
 // npm version patch
 // docker build -t gcr.io/quantal-152414/exposito-website:v0.0.3 -f deploy/Dockerfile .
 // gcloud docker -- push gcr.io/quantal-152414/exposito-website:v0.0.3
+// kubectl set image deployment exposito-website exposito-website=
 
 
 async function main() {
@@ -11,6 +12,7 @@ async function main() {
     let versionResults = await execFile('npm', ['version', 'patch'])
     await execFile('docker', ['build', '-t', versionTag, '-f', 'deploy/Dockerfile', '.'])
     await execFile('gcloud', ['docker', '--', 'push', versionTag])
+    await execFile('kubectl', ['set', 'image', 'deployment', 'exposito-website', 'exposito-website=' + versionTag])
 }
 
 
